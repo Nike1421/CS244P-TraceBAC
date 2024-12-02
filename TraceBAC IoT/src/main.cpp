4bd0f236d22b152a7635852694162d5f44da8818
Nike1421 <ommiemo54@gmail.com>
2024-12-01 23:19:59 -0800

    Added WiFi Code

--- TraceBAC IoT/src/main.cpp
+++ TraceBAC IoT/src/main.cpp
@@ -1,20 +1,30 @@
 #include <Wire.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 #include <Arduino.h>
+
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "freertos/task.h"
+#include "freertos/FreeRTOS.h"
+
 #include <MPU6050.h>
 #include <LiquidCrystal_I2C.h>
+#include <PulseSensorPlayground.h>
 #include <PulseSensorPlayground.h>
 
 #define USE_ARDUINO_INTERRUPTS false // Set-up low-level interrupts for most accurate BPM math.
 
 #define BUZZER 26
 #define RED_LED 25
-#define ALC_SENSOR 13
+#define ALC_SENSOR 32
 #define PULSE_SENSOR 33
 #define BUZZER_CHANNEL 0
 
-#define FALL_THRESHOLD 400000
+#define FALL_THRESHOLD 400
 #define SAMPLE_INTERVAL 10
-#define BEEP_DURATION 5000 
+#define BEEP_DURATION 5000
 #define BAC_THRESHOLD 3
 #define PULSE_SENSOR_THRESHOLD 550
 
@@ -24,16 +34,23 @@ MPU6050 fallDetectorMPU(0x69);
 // set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-PulseSensorPlayground pulseSensor; 
+PulseSensorPlayground pulseSensor;
 
-int pulse_analog_signal = 0;    
+int pulse_analog_signal = 0;
 int my_bpm = 0;
 float prevAccX = 0, prevAccY = 0, prevAccZ = 0;
 float adcValue = 0, val = 0, mgL = 0;
-unsigned long button_time = 0;  
+unsigned long button_time = 0;
 unsigned long last_button_time = 0;
 bool buttonInterrupted = false;
 unsigned long delayStartTime = 0;
+
+String ssid = "OM";
+String pass = "OMNAIK123";
+const int udpPort = 5000;
+const char *serverIP = "3.16.143.108";
+WiFiUDP udp; // Create a UDP object
+
 float jerkMagnitude = 0;
 
 struct Button
@@ -45,14 +62,35 @@ struct Button
 
 Button fallCheckButton = {5, 0, false};
 
-void IRAM_ATTR isr() {
+void IRAM_ATTR isr()
+{
 	button_time = millis();
-	if (button_time - last_button_time > 250) { // Debounce logic
+	if (button_time - last_button_time > 250)
+	{ // Debounce logic
 		fallCheckButton.numberKeyPresses++;
 		fallCheckButton.pressed = true;
 		last_button_time = button_time;
 		buttonInterrupted = true; // Indicate button press
 	}
+}
+
+void wifi_setup()
+{
+	// WiFi.begin(ssid, pass);
+
+	// while (WiFi.status() != WL_CONNECTED)
+	// {
+	// 	delay(500);
+	// 	Serial.print(".");
+	// }
+
+	// Serial.println("");
+	// Serial.println("WiFi connected");
+	// Serial.println("IP address: ");
+	// Serial.println(WiFi.localIP());
+	// Serial.println("MAC address: ");
+	// Serial.println(WiFi.macAddress());
+	// udp.begin(8888); // ESP32 listens on port 8888 for UDP
 }
 
 void lcd_init()
@@ -87,7 +125,7 @@ void pin_init()
 void pulse_sensor_setup()
 {
 	pulseSensor.analogInput(PULSE_SENSOR);
-	pulseSensor.blinkOnPulse(RED_LED); 
+	pulseSensor.blinkOnPulse(RED_LED);
 	pulseSensor.setThreshold(PULSE_SENSOR_THRESHOLD);
 
 	if (pulseSensor.begin())
@@ -112,8 +150,8 @@ void print_lcd(int bpm, float jerk, float mgL)
 
 void playBuzzer(int buzzerFrequency, uint8_t ledState)
 {
-	ledcWrite(BUZZER_CHANNEL, buzzerFrequency); 
-	digitalWrite(RED_LED, ledState); 
+	ledcWrite(BUZZER_CHANNEL, buzzerFrequency);
+	digitalWrite(RED_LED, ledState);
 }
 
 void checkFall()
@@ -150,12 +188,13 @@ void checkFall()
 		float currentJerkMagnitude = sqrt(jerkX * jerkX + jerkY * jerkY + jerkZ * jerkZ);
 
 		// Accumulate jerk magnitude and increment sample count
-		jerkSum += currentJerkMagnitude;
+		jerkSum += currentJerkMagnitude / 1000.0;
 		sampleCount++;
 
 		delay(10);
 		// Calculate average jerk magnitude every 5 samples
-		if (sampleCount == 5) {
+		if (sampleCount == 5)
+		{
 			jerkMagnitude = jerkSum / 5;
 			jerkSum = 0;
 			sampleCount = 0;
@@ -163,9 +202,7 @@ void checkFall()
 
 		Serial.print(">Jerk: ");
 		Serial.print(currentJerkMagnitude);
-		Serial.println();		
-
-
+		Serial.println();
 
 		// Check for fall
 		if (jerkMagnitude > FALL_THRESHOLD)
@@ -253,36 +290,72 @@ void checkPulse()
 	}
 }
 
-void fallTask(void *parameter) {
-    while (true) {
-        checkFall();
-        vTaskDelay(10 / portTICK_PERIOD_MS); // Run every 10ms
-    }
+void fallTask(void *parameter)
+{
+	while (true)
+	{
+		checkFall();
+		vTaskDelay(10 / portTICK_PERIOD_MS); // Run every 10ms
+	}
 }
 
-void alcoholTask(void *parameter) {
-    while (true) {
-        checkAlcohol();
-        vTaskDelay(100 / portTICK_PERIOD_MS); // Run every 100ms
-    }
+void alcoholTask(void *parameter)
+{
+	while (true)
+	{
+		checkAlcohol();
+		vTaskDelay(100 / portTICK_PERIOD_MS); // Run every 100ms
+	}
 }
 
-void pulseTask(void *parameter) {
-    while (true) {
-        checkPulse();
-        vTaskDelay(50 / portTICK_PERIOD_MS); // Run every 50ms
-    }
+void pulseTask(void *parameter)
+{
+	while (true)
+	{
+		checkPulse();
+		vTaskDelay(50 / portTICK_PERIOD_MS); // Run every 50ms
+	}
 }
 
+void sendUDP()
+{
+	// Create a message to send
+	String message = "{\"bpm\": " + String(my_bpm) + ", \"jerk\": " + String(jerkMagnitude) + ", \"mgL\": " + String(mgL) + "}";
+
+	// Send the UDP packet
+	udp.beginPacket(serverIP, udpPort); // Start packet
+	udp.print(message);					// Add message
+	udp.endPacket();					// Send packet
+
+	Serial.println("UDP packet sent: " + message);
+
+	delay(1000); // Send every 5 seconds
+}
+
+void udpTask(void *parameter)
+{
+	while (true)
+	{
+		sendUDP();
+		vTaskDelay(50 / portTICK_PERIOD_MS); // Run every 5 seconds
+	}
+}
 
 void setup()
 {
 	Serial.begin(115200);
 	Wire.begin();
+	
+	
+	// wifi_setup();
+	
 	analogReadResolution(10);
 
 	lcd_init();
 	pin_init();
+
+	delay(1000);
+
 	pulse_sensor_setup();
 
 	// Initialize MPU6050
@@ -290,9 +363,10 @@ void setup()
 	// Serial.println(fallDetectorMPU.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
 	// Create FreeRTOS tasks
-    xTaskCreate(fallTask, "Fall Detection Task", 2048, NULL, 1, NULL);
-    xTaskCreate(alcoholTask, "Alcohol Detection Task", 2048, NULL, 1, NULL);
-    xTaskCreate(pulseTask, "Pulse Detection Task", 2048, NULL, 1, NULL);
+	xTaskCreate(fallTask, "Fall Detection Task", 2048, NULL, 1, NULL);
+	xTaskCreate(alcoholTask, "Alcohol Detection Task", 2048, NULL, 1, NULL);
+	xTaskCreate(pulseTask, "Pulse Detection Task", 2048, NULL, 1, NULL);
+	// xTaskCreate(udpTask, "UDP Send Task", 4096, NULL, 1, NULL);
 }
 
 void loop()
