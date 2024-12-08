7f622a4c84d49e91ee6a3cd034a0a1746e63db36
Nike1421 <ommiemo54@gmail.com>
2024-12-08 04:49:21 -0800

    Final Project Changes ! Merge to Master

--- TraceBAC IoT/src/main.cpp
+++ TraceBAC IoT/src/main.cpp
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <Arduino.h>
+#include <HttpClient.h>
 
 #include "nvs.h"
 #include "nvs_flash.h"
@@ -16,42 +17,47 @@
 
 #define USE_ARDUINO_INTERRUPTS false // Set-up low-level interrupts for most accurate BPM math.
 
+// Sensor Pins
 #define BUZZER 26
 #define RED_LED 25
-#define ALC_SENSOR 32
-#define PULSE_SENSOR 33
+#define ALC_SENSOR 33
+#define PULSE_SENSOR 32
 #define BUZZER_CHANNEL 0
 
-#define FALL_THRESHOLD 400
-#define SAMPLE_INTERVAL 10
-#define BEEP_DURATION 5000
+// Sensor Thresholds
 #define BAC_THRESHOLD 3
+#define FALL_THRESHOLD 300
 #define PULSE_SENSOR_THRESHOLD 550
 
-// I2C Connections
-MPU6050 fallDetectorMPU(0x69);
+#define SAMPLE_INTERVAL 10
+#define BEEP_DURATION 5000
 
 // set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// Pulse Sensor Object
 PulseSensorPlayground pulseSensor;
 
-int pulse_analog_signal = 0;
+// Pulse Sensor Variables
 int my_bpm = 0;
-float prevAccX = 0, prevAccY = 0, prevAccZ = 0;
+int pulse_analog_signal = 0;
+
+// Alcohol Sensor Variables
 float adcValue = 0, val = 0, mgL = 0;
+
+// Accelerometer Object
+MPU6050 fallDetectorMPU(0x69);
+
+// Accelerometer Variables
+int currentIndex = 0;
+float jerkMagnitude = 0;
+float jerkValues[10] = {0};
+float prevAccX = 0, prevAccY = 0, prevAccZ = 0;
+
 unsigned long button_time = 0;
-unsigned long last_button_time = 0;
 bool buttonInterrupted = false;
 unsigned long delayStartTime = 0;
-
-String ssid = "OM";
-String pass = "OMNAIK123";
-const int udpPort = 5000;
-const char *serverIP = "3.16.143.108";
-WiFiUDP udp; // Create a UDP object
-
-float jerkMagnitude = 0;
+unsigned long last_button_time = 0;
 
 struct Button
 {
@@ -66,31 +72,41 @@ void IRAM_ATTR isr()
 {
 	button_time = millis();
 	if (button_time - last_button_time > 250)
-	{ // Debounce logic
+	{
 		fallCheckButton.numberKeyPresses++;
 		fallCheckButton.pressed = true;
 		last_button_time = button_time;
-		buttonInterrupted = true; // Indicate button press
+		buttonInterrupted = true;
 	}
 }
 
+// WIFI Variables
+WiFiUDP udp;
+
+String ssid = "OM";
+String pass = "OMNAIK123";
+const int flask_server_port = 5000;
+const char *flask_server_ip = "3.145.120.107";
+
+const int kNetworkDelay = 1000;
+const int kNetworkTimeout = 30 * 1000;
+
 void wifi_setup()
 {
-	// WiFi.begin(ssid, pass);
+	WiFi.begin(ssid, pass);
 
-	// while (WiFi.status() != WL_CONNECTED)
-	// {
-	// 	delay(500);
-	// 	Serial.print(".");
-	// }
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}
 
-	// Serial.println("");
-	// Serial.println("WiFi connected");
-	// Serial.println("IP address: ");
-	// Serial.println(WiFi.localIP());
-	// Serial.println("MAC address: ");
-	// Serial.println(WiFi.macAddress());
-	// udp.begin(8888); // ESP32 listens on port 8888 for UDP
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+	Serial.println("MAC address: ");
+	Serial.println(WiFi.macAddress());
 }
 
 void lcd_init()
@@ -118,20 +134,25 @@ void pin_init()
 
 	digitalWrite(RED_LED, LOW);
 
-	delay(5000);
-	// Replace with 20000
+	// 20 Second Sleep for Alcohol Sensor Warmup
+	delay(20000);
+
+	lcd.clear();
 }
 
 void pulse_sensor_setup()
 {
-	pulseSensor.analogInput(PULSE_SENSOR);
 	pulseSensor.blinkOnPulse(RED_LED);
+	pulseSensor.analogInput(PULSE_SENSOR);
 	pulseSensor.setThreshold(PULSE_SENSOR_THRESHOLD);
 
-	if (pulseSensor.begin())
-	{
-		Serial.println("HeartBeat Sensor found.");
-	}
+	Serial.println(pulseSensor.begin() ? "Pulse Sensor Connection Successful!" : "Pulse Sensor Connection Failed!");
+}
+
+void accelerometer_setup()
+{
+	fallDetectorMPU.initialize();
+	Serial.println(fallDetectorMPU.testConnection() ? "MPU6050 Connection Successful!" : "MPU6050 Connection Failed!");
 }
 
 void print_lcd(int bpm, float jerk, float mgL)
@@ -146,6 +167,15 @@ void print_lcd(int bpm, float jerk, float mgL)
 	lcd.setCursor(0, 1);
 	lcd.print("Jerk:");
 	lcd.print(jerk);
+}
+
+void print_lcd(String first_line, String second_line)
+{
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print(first_line);
+	lcd.setCursor(0, 1);
+	lcd.print(second_line);
 }
 
 void playBuzzer(int buzzerFrequency, uint8_t ledState)
@@ -165,7 +195,7 @@ void checkFall()
 
 		int16_t ax, ay, az;
 		static float jerkSum = 0;
-		static int sampleCount = 0;
+		float currentJerkMagnitude = 0;
 
 		fallDetectorMPU.getAcceleration(&ax, &ay, &az);
 
@@ -185,29 +215,25 @@ void checkFall()
 		prevAccZ = accelerationZ;
 
 		// Calculate jerk magnitude
-		float currentJerkMagnitude = sqrt(jerkX * jerkX + jerkY * jerkY + jerkZ * jerkZ);
+		currentJerkMagnitude = sqrt(jerkX * jerkX + jerkY * jerkY + jerkZ * jerkZ) / 1000.0;
 
-		// Accumulate jerk magnitude and increment sample count
-		jerkSum += currentJerkMagnitude / 1000.0;
-		sampleCount++;
-
-		delay(10);
-		// Calculate average jerk magnitude every 5 samples
-		if (sampleCount == 5)
-		{
-			jerkMagnitude = jerkSum / 5;
-			jerkSum = 0;
-			sampleCount = 0;
-		}
+		// Calculate Rolling Average
+		jerkSum -= jerkValues[currentIndex];
+		jerkValues[currentIndex] = currentJerkMagnitude;
+		jerkSum += currentJerkMagnitude;
+		jerkMagnitude = jerkSum / 10;
+		currentIndex = (currentIndex + 1) % 10;
 
 		Serial.print(">Jerk: ");
 		Serial.print(currentJerkMagnitude);
 		Serial.println();
 
 		// Check for fall
-		if (jerkMagnitude > FALL_THRESHOLD)
+		if (currentJerkMagnitude > FALL_THRESHOLD)
 		{
 			Serial.println("Fall detected!");
+
+			print_lcd("FALL DETECTED!", "PRESS BUTTON!");
 
 			// Play Buzzer and Set LED to HIGH
 			playBuzzer(5000, HIGH);
@@ -220,11 +246,13 @@ void checkFall()
 				if (fallCheckButton.pressed)
 				{
 					fallCheckButton.pressed = false;
+					print_lcd("BUTTON PRESSED", "BUZZER STOPPED");
 					// Serial.println("Buzzer stopped by button press.");
 					break;
 				}
 			}
 			lcd_init();
+
 			// Stop Buzzer and Set LED to LOW
 			playBuzzer(0, LOW);
 		}
@@ -233,111 +261,142 @@ void checkFall()
 
 void checkAlcohol()
 {
-	float unval = 0;
 	adcValue = 0;
+	float unval = 0;
+
 	for (int i = 0; i < 5; i++)
 	{
 		unval = analogRead(ALC_SENSOR);
 		adcValue += unval;
 		delay(10);
 	}
-	// adcValue = analogRead(ALC_SENSOR);
+
 	val = (adcValue / 5) * (5.0 / 1024.0);
 	mgL = 0.67 * val;
 
 	if (mgL > BAC_THRESHOLD)
 	{
-		// isDrunk = true;
-		// digitalWrite(BUZZER, HIGH);
-		// digitalWrite(GREEN_LED, LOW); // Turn LED off.
-		// digitalWrite(RED_LED, HIGH);  // Turn LED on.
-		// delay(300);
+		print_lcd("HIGH BAC DETECTED!", "PRESS BUTTON!");
 		playBuzzer(5000, HIGH);
-	}
-	else
-	{
-		// isDrunk = false;
-		// digitalWrite(GREEN_LED, HIGH); // Turn LED on.
-		// digitalWrite(RED_LED, LOW);    // Turn LED off.
-		// playBuzzer(0, LOW);
 	}
 	if (fallCheckButton.pressed)
 	{
 		fallCheckButton.pressed = false;
 		playBuzzer(0, LOW);
-
-		// Serial.println("Buzzer stopped by button press.");
+		print_lcd("BUTTON PRESSED", "BUZZER STOPPED");
 	}
-	Serial.print("BAC: ");
-	// Serial.print(mgL, 3);
-	// Serial.println("mg/L");
-	// playBuzzer(0, LOW);
+
+	// Send BAC to Serial Plotter
+	Serial.print(">BAC: ");
+	Serial.print(mgL);
+	Serial.println();
 }
 
 void checkPulse()
 {
+	// Read pulse sensor analog signal
 	pulse_analog_signal = analogRead(PULSE_SENSOR);
-	// Serial.print(">Signal:");
-	// Serial.print(pulse_analog_signal); // Send the Signal value to Serial Plotter.
-	// Serial.println();
 
+	// Send Pulse to Serial Plotter
+	Serial.print(">Signal:");
+	Serial.print(pulse_analog_signal);
+	Serial.println();
+
+	// Update pulse sensor
 	if (pulseSensor.sawStartOfBeat())
 	{
 		my_bpm = pulseSensor.getBeatsPerMinute();
-
-		// Serial.print("BPM: ");
-		// Serial.println(my_bpm);
 	}
 }
 
-void fallTask(void *parameter)
+void sendPOSTRequest()
 {
-	while (true)
+	int err = 0;
+	WiFiClient c;
+	HttpClient http(c);
+
+	String postData = "{\"bpm\": " + String(my_bpm) + ", \"bac\": " + String(mgL) + ", \"jerk\": " + String(jerkMagnitude) + "}";
+
+	http.beginRequest();
+	err = http.post(flask_server_ip, flask_server_port, "/api/data", NULL);
+
+	if (err == 0)
 	{
-		checkFall();
-		vTaskDelay(10 / portTICK_PERIOD_MS); // Run every 10ms
+		Serial.println("POST request started successfully");
+
+		// Add headers manually after starting the request
+		http.sendHeader("Content-Type", "application/json");
+		http.sendHeader("Content-Length", postData.length());
+
+		http.print(postData); // Send the JSON data in the body
+
+		err = http.responseStatusCode();
+		if (err >= 0)
+		{
+			Serial.print("Got status code: ");
+			Serial.println(err);
+
+			err = http.skipResponseHeaders();
+
+			if (err >= 0)
+			{
+				int bodyLen = http.contentLength();
+				Serial.print("Content length is: ");
+				Serial.println(bodyLen);
+				Serial.println();
+				Serial.println("Body returned follows:");
+
+				// Now we've got to the body, so we can print it out
+				unsigned long timeoutStart = millis();
+				char c;
+
+				// Whilst we haven't timed out & haven't reached the end of the body
+				while ((http.connected() || http.available()) &&
+					   ((millis() - timeoutStart) < kNetworkTimeout))
+				{
+					if (http.available())
+					{
+						c = http.read();
+						// Print out this character
+						Serial.print(c);
+						bodyLen--;
+						// We read something, reset the timeout counter
+						timeoutStart = millis();
+					}
+					else
+					{
+						// We haven't got any data, so let's pause to allow some to
+						// arrive
+						delay(kNetworkDelay);
+					}
+				}
+			}
+			else
+			{
+				Serial.print("Failed to skip response headers: ");
+				Serial.println(err);
+			}
+		}
+		else
+		{
+			Serial.print("Getting response failed: ");
+			Serial.println(err);
+		}
 	}
-}
-
-void alcoholTask(void *parameter)
-{
-	while (true)
+	else
 	{
-		checkAlcohol();
-		vTaskDelay(100 / portTICK_PERIOD_MS); // Run every 100ms
+		Serial.print("Connect failed: ");
+		Serial.println(err);
 	}
+	http.stop();
 }
 
-void pulseTask(void *parameter)
+void server_request_task(void *parameter)
 {
 	while (true)
 	{
-		checkPulse();
-		vTaskDelay(50 / portTICK_PERIOD_MS); // Run every 50ms
-	}
-}
-
-void sendUDP()
-{
-	// Create a message to send
-	String message = "{\"bpm\": " + String(my_bpm) + ", \"jerk\": " + String(jerkMagnitude) + ", \"mgL\": " + String(mgL) + "}";
-
-	// Send the UDP packet
-	udp.beginPacket(serverIP, udpPort); // Start packet
-	udp.print(message);					// Add message
-	udp.endPacket();					// Send packet
-
-	Serial.println("UDP packet sent: " + message);
-
-	delay(1000); // Send every 5 seconds
-}
-
-void udpTask(void *parameter)
-{
-	while (true)
-	{
-		sendUDP();
-		vTaskDelay(50 / portTICK_PERIOD_MS); // Run every 5 seconds
+		sendPOSTRequest();
+		vTaskDelay(500 / portTICK_PERIOD_MS); // Run every 5 seconds
 	}
 }
 
@@ -345,32 +404,32 @@ void setup()
 {
 	Serial.begin(115200);
 	Wire.begin();
-	
-	
-	// wifi_setup();
-	
+	wifi_setup();
+
 	analogReadResolution(10);
 
 	lcd_init();
+	print_lcd("TraceBAC CS224P", "Initializing...");
+
 	pin_init();
-
 	delay(1000);
-
 	pulse_sensor_setup();
+	accelerometer_setup();
 
-	// Initialize MPU6050
-	fallDetectorMPU.initialize();
-	// Serial.println(fallDetectorMPU.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-	// Create FreeRTOS tasks
-	xTaskCreate(fallTask, "Fall Detection Task", 2048, NULL, 1, NULL);
-	xTaskCreate(alcoholTask, "Alcohol Detection Task", 2048, NULL, 1, NULL);
-	xTaskCreate(pulseTask, "Pulse Detection Task", 2048, NULL, 1, NULL);
-	// xTaskCreate(udpTask, "UDP Send Task", 4096, NULL, 1, NULL);
+	// xTaskCreatePinnedToCore(server_request_task, "HTTP_Request_Task", 10000, NULL, 1, NULL, 0);
 }
 
 void loop()
 {
+	checkFall();
+	delay(10);
+
+	checkAlcohol();
+	delay(10);
+
+	checkPulse();
+
 	print_lcd(my_bpm, jerkMagnitude, mgL);
-	vTaskDelay(500 / portTICK_PERIOD_MS); // Update LCD every 500ms
+
+	vTaskDelay(250 / portTICK_PERIOD_MS); // Update LCD every 500ms
 }
